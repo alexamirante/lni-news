564f7ae5b02d1114f667de61646964f02d0483b7
alex <alex@meetecho.com>
2019-08-02 17:50:15 +0200

    second commit

---
+++ check-lni.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#set -x
+
+ADDRESS=https://www.lnipozzuoli.it
+
+notifyerror() {
+	echo "Error. New date: $(cat date.new); New msg: $(cat msg.new)"
+}
+
+notifychange() {
+	echo -e "$(cat date.new)\n\n$(cat msg.new)\n\n$URL" | mail -s "News dalla Lega Navale" alessandro.amirante@gmail.com
+}
+
+check4changes() {
+	curl -s $ADDRESS > home.html
+	[ ! -s home.html ] && notifyerror && exit 1
+	cat home.html | grep '<div class="latestnews-date"' | head -1 | awk -F"</i>" '{print $2}' | awk -F"</div>" '{print $1}' | xargs > date.new
+	cat home.html | grep -A1 '<span itemprop="name"' | head -2 | tail -1 | awk -F"</span>" '{print $1}' | xargs > msg.new
+	URL=$(cat home.html | grep "itemprop=\"url\"" | head -1 | awk '{print $2}' | sed "s/href=/$ADDRESS/" | tr -d \")
+	[ ! -s date.new ] && [ -z "$(cat date.new)" ] && notifyerror && exit 1
+	[ ! -s msg.new ] && [ -z "$(cat msg.new)" ] && notifyerror && exit 1
+	[ ! -f date.old ] && cp date.new date.old
+	[ ! -f msg.old ] && cp msg.new msg.old
+		
+	[ ! -z "$(diff date.old date.new)" -o ! -z "$(diff msg.old msg.new)" ] && notifychange
+	mv date.new date.old
+	mv msg.new msg.old
+	exit 0
+}
+
+check4changes || check4changes
