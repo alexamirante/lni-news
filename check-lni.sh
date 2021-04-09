e36bf6fe1fe97756e67abc8429f8bcc444bcb41e
Alessandro Amirante <alex@meetecho.com>
2021-04-09 09:12:56 +0200

    add -0 to xargs to ignore quotes in retrieved messages

--- check-lni.sh
+++ check-lni.sh
@@ -18,7 +18,7 @@ check4changes() {
 	curl -s $ADDRESS > home.html
 	[ ! -s home.html ] && notifyerror && exit 1
 	cat home.html | grep '<div class="latestnews-date"' | head -1 | awk -F"</i>" '{print $2}' | awk -F"</div>" '{print $1}' | xargs > date.new
-	cat home.html | grep -A1 '<span itemprop="name"' | head -2 | tail -1 | awk -F"</span>" '{print $1}' | xargs > msg.new
+	cat home.html | grep -A1 '<span itemprop="name"' | head -2 | tail -1 | awk -F"</span>" '{print $1}' | xargs -0 > msg.new
 	URL=$(cat home.html | grep "itemprop=\"url\"" | head -1 | awk '{print $2}' | sed "s,href=,$ADDRESS," | tr -d \")
 	[ ! -s date.new ] && [ -z "$(cat date.new)" ] && notifyerror && exit 1
 	[ ! -s msg.new ] && [ -z "$(cat msg.new)" ] && notifyerror && exit 1
